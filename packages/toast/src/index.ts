80f7ebcabc722c4f288b4d7266847a72f9d3e3b2
th1n <thinex@wp.pl>
2026-07-20 03:35:47 +0200

    feat(toast): export public api

---
+++ packages/toast/src/index.ts
@@ -0,0 +1,13 @@
+export { ToastProvider } from "./provider";
+export { Toast } from "./toast";
+export { toast } from "./toast-api";
+export type {
+  ToastIcon,
+  ToastIconStyle,
+  ToastIcons,
+  ToastInput,
+  ToastOptions,
+  ToastPosition,
+  ToastProps,
+  ToastVariant,
+} from "./types";
